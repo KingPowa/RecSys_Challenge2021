37a4c96ae3515a7a716bc69d4e77be6e12180824
Alessio <martignetti.alessio@gmail.com>
2021-12-29 16:46:40 +0100

    selectedResults

--- Notebooks/9_OptimalCBF_2.py
+++ Notebooks/9_OptimalCBF_2.py
@@ -14,9 +14,10 @@ sys.path.append('../RecSysRep/')
 import Basics.Load as ld
 
 URM_all, ICM_genre_all, ICM_subgenre_all, ICM_channel_all, ICM_event_all = ld.getCOOs()
-# URM_train, URM_val = ld.getSplit(URM_train_val, 5678, 0.8)
-ICM_length_all_7km = ld.getICMlength('7km')
-ICM_length_all_9km = ld.getICMlength('9km')
+# URM_train, URM_val = ld.getSplit(URM_train_val, 5678, 0.8
+ICM_7 = ld.getICMselected(7)
+ICM_8 = ld.getICMselected(8)
+ICM_9 = ld.getICMselected(9)
 
 # In[3]:
 
@@ -36,7 +37,7 @@ evaluator_validation = EvaluatorHoldout(URM_validation, cutoff_list=[10])
 
 import os
     
-n_cases = 100
+n_cases = 200
 n_random_starts = int(n_cases*0.3)
 metric_to_optimize = "MAP"   
 cutoff_to_optimize = 10
@@ -46,8 +47,8 @@ cutoff_to_optimize = 10
 from skopt.space import Real, Integer, Categorical
 
 hyperparameters_range_dictionary = {
-    "shrink": Integer(0, 2000),
-    "topK": Integer(10, 3000),
+    "shrink": Integer(0, 4000),
+    "topK": Integer(10, 8000),
     "feature_weighting": Categorical(["BM25", "TF-IDF", "none"]),
     "normalize": Categorical([True, False]),
 }
@@ -69,13 +70,13 @@ from HyperparameterTuning.SearchAbstractClass import SearchInputRecommenderArgs
   
 
 recommender_input_args = SearchInputRecommenderArgs(
-    CONSTRUCTOR_POSITIONAL_ARGS = [URM_train, ICM_length_all_7km],     # For a CBF model simply put [URM_train, ICM_train]
+    CONSTRUCTOR_POSITIONAL_ARGS = [URM_train, ICM_7],     # For a CBF model simply put [URM_train, ICM_train]
     CONSTRUCTOR_KEYWORD_ARGS = {},
     FIT_POSITIONAL_ARGS = [],
     FIT_KEYWORD_ARGS = {}     # Additiona hyperparameters for the fit function
 )
 
-output_folder_path = "../result_experiments/9_ItemKNN_length_7km/"
+output_folder_path = "../result_experiments/9_ItemKNN_selected_7/"
 
 # If directory does not exist, create
 if not os.path.exists(output_folder_path):
@@ -94,13 +95,13 @@ hyperparameterSearch.search(recommender_input_args,
 
 
 recommender_input_args = SearchInputRecommenderArgs(
-    CONSTRUCTOR_POSITIONAL_ARGS = [URM_train, ICM_length_all_9km],     # For a CBF model simply put [URM_train, ICM_train]
+    CONSTRUCTOR_POSITIONAL_ARGS = [URM_train, ICM_8],     # For a CBF model simply put [URM_train, ICM_train]
     CONSTRUCTOR_KEYWORD_ARGS = {},
     FIT_POSITIONAL_ARGS = [],
     FIT_KEYWORD_ARGS = {}     # Additiona hyperparameters for the fit function
 )
 
-output_folder_path = "../result_experiments/9_ItemKNN_length_9km/"
+output_folder_path = "../result_experiments/9_ItemKNN_selected_8/"
 
 # If directory does not exist, create
 if not os.path.exists(output_folder_path):
@@ -117,6 +118,30 @@ hyperparameterSearch.search(recommender_input_args,
                        cutoff_to_optimize = cutoff_to_optimize,
                       )
 
+
+recommender_input_args = SearchInputRecommenderArgs(
+    CONSTRUCTOR_POSITIONAL_ARGS = [URM_train, ICM_9],     # For a CBF model simply put [URM_train, ICM_train]
+    CONSTRUCTOR_KEYWORD_ARGS = {},
+    FIT_POSITIONAL_ARGS = [],
+    FIT_KEYWORD_ARGS = {}     # Additiona hyperparameters for the fit function
+)
+
+output_folder_path = "../result_experiments/9_ItemKNN_selected_9/"
+
+# If directory does not exist, create
+if not os.path.exists(output_folder_path):
+    os.makedirs(output_folder_path)
+
+
+hyperparameterSearch.search(recommender_input_args,
+                       hyperparameter_search_space = hyperparameters_range_dictionary,
+                       n_cases = n_cases,
+                       n_random_starts = n_random_starts,
+                       output_folder_path = output_folder_path, # Where to save the results
+                       output_file_name_root = recommender_class.RECOMMENDER_NAME, # How to call the files
+                       metric_to_optimize = metric_to_optimize,
+                       cutoff_to_optimize = cutoff_to_optimize,
+                      )
 
 
 
